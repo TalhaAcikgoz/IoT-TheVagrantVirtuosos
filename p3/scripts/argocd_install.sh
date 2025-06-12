78ed2630d13d334bbc6fce995321e1db5bbe989d
TalhaAcikgoz <89697506+TalhaAcikgoz@users.noreply.github.com>
2025-06-12 22:54:57 +0300

    Update argocd_install.sh

--- p3/scripts/argocd_install.sh
+++ p3/scripts/argocd_install.sh
@@ -46,7 +46,7 @@ docker pull ghcr.io/dexidp/dex
 ################################################################################
 if ! command -v kubectl >/dev/null 2>&1; then
   echo "==> kubectl indiriliyor..."
-  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  curl -LO "https://dl.k8s.io/release/v1.33.0/bin/linux/amd64/kubectl"
   chmod +x kubectl
   sudo mv kubectl /usr/local/bin/
 else
